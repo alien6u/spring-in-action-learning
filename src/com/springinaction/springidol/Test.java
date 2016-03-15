b7d2f62bc545774c408f92d89839e591a5b7ed08
alien6u <1218498720@qq.com>
2016-03-15 18:48:44 +0800

    declare beans

---
+++ src/com/springinaction/springidol/Test.java
@@ -0,0 +1,17 @@
+package com.springinaction.springidol;
+
+import static org.junit.Assert.*;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");
+		Perfomer perfomer = (Perfomer)ctx.getBean("poeticDuke");
+		perfomer.perform();
+	}
+
+}
