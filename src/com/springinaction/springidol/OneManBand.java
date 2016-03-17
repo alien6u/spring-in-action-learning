d4e1712b6bb7e382219ee809aaba61214e884ee7
alien6u <1218498720@qq.com>
2016-03-17 15:28:48 +0800

    add jar

---
+++ src/com/springinaction/springidol/OneManBand.java
@@ -0,0 +1,22 @@
+package com.springinaction.springidol;
+
+import java.util.Collection;
+
+import com.sun.xml.internal.fastinfoset.sax.Properties;
+
+public class OneManBand implements Perfomer{
+
+	public OneManBand(){
+		
+	}
+	@Override
+	public void perform() {
+
+		
+	}
+	private Properties instruments;
+	public void SetInstruments(Properties instruments){
+		this.instruments = instruments;
+	}
+
+}
